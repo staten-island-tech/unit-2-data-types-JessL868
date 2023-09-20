5e90905f11b55f866f45cc26971d1010e40f02c6
JessL868 <jessicaliu868@gmail.com>
2023-09-19 20:51:51 -0400

    Home Coding 4

--- app3.py
+++ app3.py
@@ -7,4 +7,8 @@ print(values)
 for i in values:
     print(i)
 
-print(values[7])
+print(values[6])
+
+
+food = ["pizza", "taco", "bread", ""]
+print("It was {food[0]} day at school, and {name} was super {adjective} for lunch. But when she went outside to eat, a {noun} stole her {food}! {name} chased the {noun} all over school. She {verb1}, {verb2}, and {verb3} through the playground. Then she tripped on her {noun} and the {noun} escaped. Luckily, {name}'s friends were willing to share their {food} with her.")
