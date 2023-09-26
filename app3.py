b72d514cf44c54ea259425985f9dee8591dfec2b
JessL868 <jessicaliu868@gmail.com>
2023-09-26 13:59:20 -0400

    Lesson 10

--- app3.py
+++ app3.py
@@ -1,29 +1,45 @@
-x = 3
-y = float(3)
-print(x,y)
+def floats():
+    x = 3
+    y = float(3)
+    print(x,y)
 
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
+def lists():
+        values = [1,2.23,5,7,2,30,15]
+        print(values)
+        for i in values:
+            print(i)
+        print(values[6])
 
-print(values[6])
+def string_methods():
+    x = "this is a thing"
+    y = x.split( )
+    z = y[0]
+    print(y)
+    print(z)
 
-x = "this is a thing"
-y = x.split( )
-z = y[0]
-print(y)
-print(z)
+def how_many_words():
+    b = input("please put a sentence")
+    c = b.split() 
+    print(len(c))
 
-b = input("please put a sentence")
-c = b.split()
-print(len(c))
+def odd_or_even():
+    a = 60 % 2
+    if a == 0:
+        print("even")
+    else: 
+        print("odd")
 
-a = 60 % 2
-if a == 0:
-    print("even")
-else: 
-    print("odd")
+def bill():
+    m = input("What tip would you like to offer?")
+    if m == "0%":
+         print("bad")
+    elif m == "15%":
+        print("okay")
+    elif m == "20%":
+        print("good")
+    elif m == ("25%"):
+        print("great")
 
-
-
+def factors():
+    a = input("please type a number")
+    if a =  
